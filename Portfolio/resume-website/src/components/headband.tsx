ada74f461191bcd44a9559218e518cfe49fb44db
Reed Gehling <raymanreed@gmail.com>
2026-02-10 18:09:58 -0800

    turning navigation headband into headband

--- Portfolio/resume-website/src/components/headband.tsx
+++ Portfolio/resume-website/src/components/headband.tsx
@@ -2,33 +2,35 @@ import { NavLink } from "react-router"
 
 function Headband() {
     return (
-        <nav>
-            <h5>
-                <NavLink to="/">
-                    Home
-                </NavLink>
-            </h5>
-            <h5>
-                <NavLink to="test">
-                    Test Page
-                </NavLink>
-            </h5>
-            <h5>
-                <NavLink to="nested">
-                    Nested Page
-                </NavLink>
-            </h5>
-            <h5>
-                <NavLink to="stories">
-                    Stories
-                </NavLink>
-            </h5>
-            <h5>
-                <NavLink to="projects">
-                    Projects
-                </NavLink>
-            </h5>
-        </nav>
+        <div className="headband-bar">
+            <nav className="headband-item-container">
+                <div className="headband-item-1 home-link headband-nav-item">
+                    <NavLink to="/">
+                        Home
+                    </NavLink>
+                </div>
+                <div className="headband-item-2 headband-nav-item">
+                    <NavLink to="test">
+                        Test Page
+                    </NavLink>
+                </div>
+                <div className="headband-item-3 headband-nav-item">
+                    <NavLink to="nested">
+                        Nested Page
+                    </NavLink>
+                </div>
+                <div className="headband-item-4 stories-link headband-nav-item">
+                    <NavLink to="stories">
+                        Stories
+                    </NavLink>
+                </div>
+                <div className="headband-item-5 project-link headband-nav-item">
+                    <NavLink to="projects">
+                        Projects
+                    </NavLink>
+                </div>
+            </nav>
+        </div>
     )
 }
 
